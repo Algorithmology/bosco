2e916cd806e9ef9405e43135d62f3ff818f0a185
KellerLiptrap <97915015+KellerLiptrap@users.noreply.github.com>
2024-04-26 11:08:37 -0400

    added doc strings

--- bosco/list-processing/sorting.py
+++ bosco/list-processing/sorting.py
@@ -2,7 +2,10 @@ from typing import List
 
 """List sorting algorithms."""
 
+from typing import List
+
 def bubble_sort(arr: List[int]) -> List[int]:
+    """Sorts a list of integers using the bubble sort algorithm."""
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
@@ -11,6 +14,7 @@ def bubble_sort(arr: List[int]) -> List[int]:
     return arr
 
 def selection_sort(arr: List[int]) -> List[int]:
+    """Sorts a list of integers using the selection sort algorithm."""
     n = len(arr)
     for i in range(n):
         min_idx = i
@@ -21,6 +25,7 @@ def selection_sort(arr: List[int]) -> List[int]:
     return arr
 
 def insertion_sort(arr: List[int]) -> List[int]:
+    """Sorts a list of integers using the insertion sort algorithm."""
     n = len(arr)
     for i in range(1, n):
         key = arr[i]
@@ -32,6 +37,7 @@ def insertion_sort(arr: List[int]) -> List[int]:
     return arr
 
 def merge_sort(arr: List[int]) -> List[int]:
+    """Sorts a list of integers using the merge sort algorithm."""
     if len(arr) > 1:
         mid = len(arr) // 2
         L = arr[:mid]

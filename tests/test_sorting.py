945acbbca26d335008e91bdfcead0d99963513d5
Ochirsaikhan <davaajambal01@allegheny.edu>
2024-04-29 08:30:51 -0400

    Add more test cases

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -133,3 +133,77 @@ class TestSelectionSort:
 
         # Assert
         assert array == expected_result
+
+class TestHeapify:
+
+    # Given an array with one element, the function should return the same array
+    def test_array_with_one_element(self):
+        # Arrange
+        array = [5]
+    
+        # Act
+        sorting.heapify(array, len(array), 0)
+    
+        # Assert
+        assert array == [5]
+
+    # Given an empty array, the function should return an empty array
+    def test_empty_array(self):
+        # Arrange
+        array = []
+    
+        # Act
+        sorting.heapify(array, len(array), 0)
+    
+        # Assert
+        assert array == []
+
+class TestShellSort:
+
+    # The function sorts a list of integers in ascending order.
+    def test_sorts_list_in_ascending_order(self):
+        # Arrange
+        array = [4, 2, 7, 1, 5]
+    
+        # Act
+        result = sorting.shell_sort(array)
+    
+        # Assert
+        assert result == [1, 2, 4, 5, 7]
+
+    # The function sorts an empty list.
+    def test_sorts_empty_list(self):
+        # Arrange
+        array = []
+    
+        # Act
+        result = sorting.shell_sort(array)
+    
+        # Assert
+        assert result == []
+
+class TestBucketSort:
+
+    # sorts a list of positive integers in ascending order
+    def test_sort_positive_integers(self):
+        # Arrange
+        arr = [5, 2, 8, 1, 9]
+        expected = [1, 2, 5, 8, 9]
+
+        # Act
+        result = sorting.bucket_sort(arr)
+
+        # Assert
+        assert result == expected
+
+    # sorts an empty list
+    def test_sort_empty_list(self):
+        # Arrange
+        arr = []
+        expected = []
+
+        # Act
+        result = sorting.bucket_sort(arr)
+
+        # Assert
+        assert result == expected
